74e744d0be87637a3d6665fa7ef57210a79e817b
Shreyas200408 <shreyasshreyas0132@gmail.com>
2025-07-19 13:07:23 +0530

    Add files via upload
    I am very much excited to share my day 1 python class .
    Python is one of the most powerful open source tool , which is play a major roll in Data Science .

---
+++ python assignment.py
@@ -0,0 +1,199 @@
+# place="banglore"
+# amount=120
+# distance=140.7
+# destination="mysore"
+# print(place)
+# print("my location is",place)
+# print("ticket price is",amount)
+# print("travalling distance from banglore t0 mysore ",distance)
+# print(destination)
+# a="are you boy??"
+# print(a)
+# print(a[1])
+# print(a[2:])
+# print(a[:])
+# print(a[-1])
+# print(a[-3])
+# print(a[-3:])
+# print(a[::-1])
+# a="boy"
+# print(a.replace("b","B"))
+# s=a.replace("b","B")
+# print(s)
+# a.replace("b","B")
+# print(a)
+# place2="mysore is clean city"
+# print(len(place2))
+# name="shreyas"
+# age=19
+# weight=50.3
+# print("student name is %s" %name)
+# print("student age is %d" %age)
+# print("student weight is %f" %weight)
+# print(name.split("s"))
+# print(f"name is{name}")
+# name1="shreyas"
+# name2="akshay"
+# space=" "
+# conactenation=name1+space+name2
+# print(conactenation)
+
+# name="shreyas"
+# print(name)
+# description="i am shreyas complited my UG BCA in jss college of arts,commerce and science ooty road mysore "
+# print(description)
+
+# # m= int(input("enter the first number:"))
+# # n=int(input("enter the second number:"))
+# # sum=m+n
+# # print("the sum is",sum)
+
+# a="python"
+# b=12.3
+# c=12
+# d="34.6"
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+
+# s="JSS"
+# i="college"
+# conactenation=s+i
+# print(conactenation)
+# space=" "
+# conactenation=s+space+i
+# print(conactenation)
+
+# list=[145,"float",89.0,["list",9.8,78],("integer",78,7.09)]
+# print(list)
+# print(list[2])
+# print(list[3][1])
+# print(list[-2][1])
+# print(list[3:])
+# list[2]=89
+# print(list)
+# print(list[::-1])
+
+# bikes=["rx","ktm","bulet","dio"]
+# bikes.append("active")
+# print(bikes)
+
+# bikes.insert(2,"ns")
+# print(bikes)
+
+# bikes.clear()
+# print(bikes)
+
+# bikes.pop()
+# print(bikes)
+
+# list=[45,"float",89.0]
+# list1=["list",9.8,78]
+# con=list+list1
+# print(con)
+# list=[12,4.5]*5
+# print(list)
+
+# tuple=("tup",89.90,896,["list",89.56,900])
+# print(tuple)
+# print(tuple[3])
+# print(tuple[3][2])
+# print(tuple[-1][2])
+# print(tuple[0:4])
+# print(tuple[::-1])
+# tuple[2]=56
+# print(tuple)
+
+# number=[12,34.6,"jai"]
+# s=45
+# r=float(s)
+# print(r)
+# t=int(r)
+# print(t)
+# w=str(t)
+# print(w)
+# print(type(w))
+
+# number=[12,35,"68",8.29]
+# print(tuple(number))
+
+# a=(12,35,"68",8.29)
+# print(a.index(8.29))
+# print(a.index(35))
+# print(a.index(12))
+# print(a.count(3))
+# a.len(a)
+# print(a)
+# min(a)
+# print(a)
+# max(a)
+# print(a)
+# del(a)
+# print(a)
+# tup=(1,2,"hello",[1.2],(5,2.2))
+# min(tup)
+# print(tup)
+# tuple=(23,"jackason",5.6)
+# list=list(tuple)
+# print(list)
+
+# print("Name: Shreyas")
+# print("Favorite Color: red")
+
+# text = input("Enter a string: ")
+# print(len(text))
+
+# print(" Simple Calculator - Addition")
+# number1 = int(input("Enter first number: "))
+# number2 = int(input("Enter second number: "))
+# print("Sum:", number1 + number2)
+
+# numbers = input("Enter numbers separated by space: ")
+# num_list = list(map(int, numbers.split()))
+# print("Sum of numbers:", sum(num_list))
+
+# age = int(input("Enter your age: "))
+# current_year = 2025
+# birth_year = current_year - age
+# print("You were born in:", birth_year)
+
+# sentence = input("Enter a sentence: ")
+# words = sentence.split()
+# for word in words:
+#     print(word)
+
+# def reverse_string(s):
+#     return s[::-1]
+
+# text = input("Enter a string to reverse: ")
+# print("Reversed string:", reverse_string(text))
+
+# celsius = float(input("Enter temperature in Celsius: "))
+# fahrenheit = (celsius * 9/5) + 32
+# print("Temperature in Fahrenheit:", fahrenheit)
+
+# words = input("Enter words separated by space: ").split()
+# shortest = min(words, key=len)
+# print("Shortest word:", shortest)
+
+# number = int(input("Enter a number: "))
+# if number % 2 == 0:
+#     print("The number is even.")
+# else:
+#     print("The number is odd.")
+
+# set={12,34,12,34,6.7}
+# print(set)
+# set.add(23)
+# print(set)
+# # set.clear()
+# # print(set)
+# set1={18,56,78,34,6.7}
+# print(set-set1)
+# print(set|set1)
+# print(set&set1)
+# set.pop()
+# print(set)
+# set.remove(23)
+# print(set)
